9b6d38969a1c6bb45083a4c42e2ac764f99dfa82
Kevin Mougammadaly <kevin.mougammadaly@gmail.com>
2019-05-19 00:04:18 +0200

    feat(infra): Update travis to deploy project

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -1,3 +1,7 @@
 export const environment = {
+  environment: 'develop',
   production: false,
+  domain_server: 'develop',
+  application_name: 'memo-riz',
+  status_code_server_down: 0, // todo change the status code
 };
